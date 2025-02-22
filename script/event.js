a3632bb1bd81874703bf44ef8ba55d6d16f26570
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-22 22:16:31 +0600

    add event Listener added

--- script/event.js
+++ script/event.js
@@ -18,3 +18,29 @@ btnPurple.onclick = makePurple;
 function makePurple(){
     document.body.style.background = 'purple';
 }
+
+document.getElementById('make-gold').addEventListener('click', function makeGold(){
+    document.body.style.backgroundColor = 'gold';
+});
+
+document.getElementById('make-tomato').addEventListener('click', function (){
+    document.body.style.backgroundColor = 'tomato';
+});
+
+// Before
+const beforeSubmit = document.getElementById('before-submit');
+// beforeSubmit.style.display = 'none';
+
+// After
+const afterSubmit = document.getElementById('after-submit');
+afterSubmit.style.display = 'none';
+
+// Submit btn
+const submitBtn = document.getElementById('submit-btn');
+
+submitBtn.addEventListener( 'click', function (){
+    beforeSubmit.style.display = 'none';
+    afterSubmit.style.display = 'block';
+    afterSubmit.style.color = 'red';
+    afterSubmit.style.fontSize = '16px';
+} )
